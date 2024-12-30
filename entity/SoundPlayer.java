ebe08d7e0b7ff98e403ccec8fb4b1053c81d330d
SitrakaNiaina <sitraka11raharilovatiana@gmail.com>
2024-12-30 22:48:32 +0100

    add sound player classes

---
+++ entity/SoundPlayer.java
@@ -0,0 +1,21 @@
+package entity;
+
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+
+public class SoundPlayer {
+
+    public static void playSound(String filePath) {
+        try {
+            File audioFile = new File(filePath);
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioFile);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioStream);
+            clip.start();
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
